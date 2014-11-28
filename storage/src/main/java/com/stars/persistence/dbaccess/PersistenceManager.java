fac238c1ac71aea5e7c7606e5d2f45a333ab5f22
sunny panjwani <panjwani@yahoo-inc.com>
2014-11-27 23:18:00 -0800

    temporary fix for data not saving to db due to multiple sessions

--- storage/src/main/java/com/stars/persistence/dbaccess/PersistenceManager.java
+++ storage/src/main/java/com/stars/persistence/dbaccess/PersistenceManager.java
@@ -7,103 +7,85 @@ import org.hibernate.Transaction;
 import java.util.logging.Logger;
 
 public class PersistenceManager {
-	  private Session session = null;
-	  private Transaction transaction = null;
-	  private SessionFactory sessionFactory = null;
+	
+	private static Session session = null;
+	private Transaction transaction = null;
+	private SessionFactory sessionFactory = null;
+	private static Logger logger = Logger.getLogger(QuerySpec.class.getName());
 
-	  private static Logger logger = Logger.getLogger(QuerySpec.class.getName());
+	public PersistenceManager(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
-	  public PersistenceManager(SessionFactory sessionFactory)
-	  {
-	    this.sessionFactory = sessionFactory;
-	  }
+	public Session getSession() {
+		if (session == null) {
+			session = createSession();
+		}
+		return session;
+	}
 
-	  public Session getSession()
-	  {
-	    if (session == null)
-	    {
-	        this.session = createSession();
-	    }
-	    return session;
-	  }
+	private Session createSession() {
+		
+		logger.info("Opening a new session");
+		Session session = this.sessionFactory.openSession();
+		return session;
+	}
 
-	  private Session createSession()
-	  {
-	    Session session = this.sessionFactory.openSession();
-	    return session;
-	  }
+	public void cleanUp() {
+		try {
+			logger.info("Hibernate transaction cleaning up.");
 
-	  public void cleanUp()
-	  {
-	    try
-	    {
-	      logger.info("Hibernate transaction cleaning up.");
+			if (this.session != null) {
+				this.session.close();
+			}
 
-	      if (this.session != null)
-	      {
-	        this.session.close();
-	      }
+			logger.info("Hibernate transaction cleaning up.");
+		} catch (Exception e) {
+			logger.info("Failed to close session");
+		}
+	}
 
-	      logger.info("Hibernate transaction cleaning up.");
-	    } catch (Exception e)
-	    {
-	      logger.info("Failed to close session");
-	    }
-	  }
+	private boolean isTransactionOpened() {
+		if ((transaction != null) && transaction.isActive()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-	  private boolean isTransactionOpened()
-	  {
-	    if ((transaction != null) && transaction.isActive())
-	    {
-	      return true;
-	    } else
-	    {
-	      return false;
-	    }
-	  }
+	public void beginTransaction() {
+		logger.info("Entering beginTransaction()");
+		logger.info("Hibernate transaction opened.");
+		logger.info("Working with session " + getSession().hashCode());
+		transaction = getSession().beginTransaction();
+		logger.info("Exiting beginTransaction()");
+	}
 
-	  public void beginTransaction()
-	  {
-	    logger.info("Entering beginTransaction()");
-	    logger.info("Hibernate transaction opened.");
-	    logger.info("Working with session " + getSession().hashCode());
-	    transaction = getSession().beginTransaction();
-	    logger.info("Exiting beginTransaction()");
-	  }
+	public void commitTransaction() {
+		if (isTransactionOpened()) {
+			transaction.commit();
+			logger.info("Hibernate transaction committed.");
+		} else {
+			logger.info("[PROD_OPS_PAGE]: Commit called without an open transaction");
+		}
+	}
 
-	  public void commitTransaction()
-	  {
-	    if (isTransactionOpened())
-	    {
-	      transaction.commit();
-	      logger.info("Hibernate transaction committed.");
-	    } else
-	    {
-	      logger.info("[PROD_OPS_PAGE]: Commit called without an open transaction");
-	    }
-	  }
+	public void rollbackTransaction() {
+		if (isTransactionOpened()) {
+			transaction.rollback();
+			logger.info("Hibernate transaction rolled back.");
+		}
+	}
 
-	  public void rollbackTransaction()
-	  {
-	    if (isTransactionOpened())
-	    {
-	      transaction.rollback();
-	      logger.info("Hibernate transaction rolled back.");
-	    }
-	  }
+	public void save(Object o) {
+		getSession().save(o);
+	}
 
-	  public void save(Object o)
-	  {
-	    getSession().save(o);
-	  }
+	public void saveOrUpdate(Object o) {
+		getSession().saveOrUpdate(o);
+	}
 
-	  public void saveOrUpdate(Object o)
-	  {
-	    getSession().saveOrUpdate(o);
-	  }
-
-	  public void update(Object o)
-	  {
-	    getSession().update(o);
-	  }
+	public void update(Object o) {
+		getSession().update(o);
+	}
 }
--- storage/src/main/java/com/stars/persistence/dbaccess/PersistenceManagerFactory.java
+++ storage/src/main/java/com/stars/persistence/dbaccess/PersistenceManagerFactory.java
@@ -4,74 +4,67 @@ import java.io.File;
 import java.util.logging.Logger;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-
+import org.hibernate.service.ServiceRegistry;
 
 public class PersistenceManagerFactory {
-	private static Logger log = Logger.getLogger(PersistenceManagerFactory.class.getName());
-	  private static PersistenceManagerFactory factory = null;
-	  private static SessionFactory appSessionFactory = null;
-	  private static SessionFactory auditLogSessionFactory = null;
-	  public static final String PERSISTENCE_HIBERNATE_CFG_FILE = "/Library/Tomcat/webapps/StarsServer-0.0.1/WEB-INF/classes/hibernate.cfg.xml";
-	  private static Configuration config = null;
-	  private static String hibernateConfigFile = null;
 
-	  private PersistenceManagerFactory()
-	  {
-		 config.addAnnotatedClass(com.stars.persistence.dao.Users.class);
-	  }
+	private static Logger log = Logger
+			.getLogger(PersistenceManagerFactory.class.getName());
+	private static PersistenceManagerFactory factory = null;
+	private static SessionFactory appSessionFactory = null;
+	private static SessionFactory auditLogSessionFactory = null;
+	public static final String PERSISTENCE_HIBERNATE_CFG_FILE = "/Users/panjwani/Desktop/eclipse/apache-tomcat-7.0.50/webapps/stars_service-0.0.1/WEB-INF/classes/hibernate.cfg.xml";
+	private static Configuration config = null;
+	private static String hibernateConfigFile = null;
 
-	  @SuppressWarnings("deprecation")
-	synchronized public static PersistenceManagerFactory getInstance()
-	  {
-	    if (config == null)
-	    {
-	    	log.info("Getting config");
-	      config = getConfiguration();
-	      factory = new PersistenceManagerFactory();
-	      log.info("Got factory");
-	      appSessionFactory = config.buildSessionFactory();
-	      auditLogSessionFactory = config.buildSessionFactory();
-	    }
-	    log.info("returning factory");
-	    return factory;
-	  }
+	private PersistenceManagerFactory() {
+		config.addAnnotatedClass(com.stars.persistence.dao.Users.class);
+	}
 
-	  static Configuration getConfiguration()
-	  {
-	    if (hibernateConfigFile == null)
-	    {
-	      hibernateConfigFile = System.getProperty(PERSISTENCE_HIBERNATE_CFG_FILE);
-	    }
+	synchronized public static PersistenceManagerFactory getInstance() {
+		
+		if (config == null) {
+			config = getConfiguration();
+			log.info("creating new persistence mgr factory");
+			factory = new PersistenceManagerFactory();
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+					.applySettings(config.getProperties()).build();
+			appSessionFactory = config.buildSessionFactory(serviceRegistry);
+			auditLogSessionFactory = appSessionFactory;
+			log.info("persistence mgr factory created successfully");
+		}
+		return factory;
+	}
 
-	    if (hibernateConfigFile != null)
-	    {
-	      File file = new File(hibernateConfigFile);
-	      if (file.exists() && file.canRead())
-	      {
-	        return new Configuration().configure(file);
-	      }
-	    }
+	static Configuration getConfiguration() {
 
-	    return new Configuration().configure();
-	  }
+		hibernateConfigFile = PERSISTENCE_HIBERNATE_CFG_FILE;
 
-	  public PersistenceManager getPersistenceManager()
-	  {
-	    return new PersistenceManager(appSessionFactory);
-	  }
+		if (hibernateConfigFile != null) {
+			File file = new File(hibernateConfigFile);
+			if (file.exists() && file.canRead()) {
+				return new Configuration().configure(file);
+			}
+		}
 
-	  public PersistenceManager getAuditLogPersistenceManager()
-	  {
-	    return new PersistenceManager(auditLogSessionFactory);
-	  }
+		return new Configuration().configure();
+	}
 
-	  /**
-	   * @param hibernateConfigFile
-	   *          the hibernateConfigFile to set
-	   */
-	  public static final void setHibernateConfigFile(String hibernateConfigFile)
-	  {
-	    PersistenceManagerFactory.hibernateConfigFile = hibernateConfigFile;
-	  }
+	public PersistenceManager getPersistenceManager() {
+		return new PersistenceManager(appSessionFactory);
+	}
+
+	public PersistenceManager getAuditLogPersistenceManager() {
+		return new PersistenceManager(auditLogSessionFactory);
+	}
+
+	/**
+	 * @param hibernateConfigFile
+	 *            the hibernateConfigFile to set
+	 */
+	public static final void setHibernateConfigFile(String hibernateConfigFile) {
+		PersistenceManagerFactory.hibernateConfigFile = hibernateConfigFile;
+	}
 }
