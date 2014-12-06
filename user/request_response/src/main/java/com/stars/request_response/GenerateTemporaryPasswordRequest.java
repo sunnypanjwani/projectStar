e6e1776b72971f22be6bf58920bd3ef31336e6e9
System Administrator <root@Ednitpreets-MacBook-Air.local>
2014-12-06 10:55:44 -0800

    User management changes

---
+++ user/request_response/src/main/java/com/stars/request_response/GenerateTemporaryPasswordRequest.java
@@ -0,0 +1,18 @@
+package com.stars.request_response;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "GenerateTemporaryPasswordRequest")
+public class GenerateTemporaryPasswordRequest{
+	private String screenName;
+	private String password;
+	
+	@XmlElement(name = "screenName")
+	public String getScreenName() {
+		return screenName;
+	}
+	public void setScreenName(String screenName) {
+		this.screenName = screenName;
+	}
+}
